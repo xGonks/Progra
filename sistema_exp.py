69bc3686336868386e9c7abe66da67aa914f7797
Gonzalo Higuera <gonzalomakenly22@gmail.com>
2025-04-25 18:58:19 -0600

    kkck

--- sistema_exp.py
+++ sistema_exp.py
@@ -1,32 +1,82 @@
 # ========================================
 # Importación de librerías
 # ========================================
-from utils import *
+
+
 from logic import *
+from utils import *
+
+
+def interpretar_glucosa(nivel):
+    if nivel > 125:
+        return "GlucosaAlta"
+    elif nivel < 70:
+        return "GlucosaBaja"
+    else:
+        return "GlucosaNormal"
+
+
+def interpretar_hemoglobina(nivel):
+    if nivel < 12:
+        return "HemoglobinaBaja"
+    elif nivel > 18:
+        return "HemoglobinaAlta"
+    else:
+        return "HemoglobinaNormal"
+
+
+def interpretar_colesterol(nivel):
+    if nivel > 200:
+        return "ColesterolAlto"
+    else:
+        return "ColesterolNormal"
+
+
+def interpretar_trigliceridos(nivel):
+    if nivel > 150:
+        return "TrigliceridosAltos"
+    else:
+        return "TrigliceridosNormales"
+
+
+def interpretar_creatinina(nivel):
+    if nivel > 1.3:
+        return "CreatininaAlta"
+    else:
+        return "CreatininaNormal"
+
 
 # ========================================
 # General knowledge
 # ========================================
+
+
 clauses = []
 
 # Ejemplo de uso
-"""# Diseases
-clauses.append(expr("Sintoma(x, CuerpoCortado) & Sintoma(x, Escurrimiento) ==> Enfermo(x, Resfrio)"))
-clauses.append(expr("Sintoma(x, Tos) & Sintoma(x, Fiebre) ==> Enfermo(x, Covid19)"))
-clauses.append(expr("Sintoma(x, CuerpoCortado) & Sintoma(x, Fiebre) ==> Enfermo(x, Covid19)"))
-clauses.append(expr("Sintoma(x, Escurrimiento) & Sintoma(x, Fiebre) ==> Enfermo(x, Covid19)"))
-clauses.append(expr("Sintoma(x, PositivoExamenCovid19) ==> Enfermo(x, Covid19)"))
-clauses.append(expr("Sintoma(x, Cansancio) & Sintoma(x, Sueno) ==> Enfermo(x, Flojera)"))
+# Diseases
+clauses.append(expr(
+    "Sintoma(x, CuerpoCortado) & Sintoma(x, Escurrimiento) ==> Enfermo(x, Resfrio)"))
+clauses.append(
+    expr("Sintoma(x, Tos) & Sintoma(x, Fiebre) ==> Enfermo(x, Covid19)"))
+clauses.append(
+    expr("Sintoma(x, CuerpoCortado) & Sintoma(x, Fiebre) ==> Enfermo(x, Covid19)"))
+clauses.append(
+    expr("Sintoma(x, Escurrimiento) & Sintoma(x, Fiebre) ==> Enfermo(x, Covid19)"))
+clauses.append(
+    expr("Sintoma(x, PositivoExamenCovid19) ==> Enfermo(x, Covid19)"))
+clauses.append(
+    expr("Sintoma(x, Cansancio) & Sintoma(x, Sueno) ==> Enfermo(x, Flojera)"))
 
 # Treatments for diseases
 clauses.append(expr("Enfermo(x, Resfrio) ==> Tratamiento(x, Reposo)"))
 clauses.append(expr("Enfermo(x, Resfrio) ==> Tratamiento(x, Liquidos)"))
 clauses.append(expr("Enfermo(x, Covid19) ==> Tratamiento(x, Reposo)"))
 clauses.append(expr("Enfermo(x, Covid19) ==> Tratamiento(x, Antivirales)"))
-clauses.append(expr("Enfermo(x, Flojera) ==> Tratamiento(x, Dormir)"))"""
+clauses.append(expr("Enfermo(x, Flojera) ==> Tratamiento(x, Dormir)"))
 
 # ========================================
-# Cláusulas de Makenly (síntomas comunes)
+# Cláusulas de Makenly
 # ========================================
 # Síntomas Normales
 clauses.append(expr("Sintoma(x, DolorCabeza) ==> Enfermo(x, Migraña)"))
@@ -89,7 +139,7 @@ kb = FolKB(clauses)
 name = input("¿Cuál es tu nombre? ")
 
 # Ejemplo de uso
-"""# Preguntas normales de síntomas
+# Preguntas normales de síntomas
 if input("¿Tienes cuerpo cortado? ").lower() == 's':
     kb.tell(expr("Sintoma({}, CuerpoCortado)".format(name)))
 if input("¿Tienes escurrimiento? ").lower() == 's':
@@ -99,72 +149,62 @@ if input("¿Tienes sueño? ").lower() == 's':
 if input("¿Tienes fiebre? ").lower() == 's':
     kb.tell(expr("Sintoma({}, Fiebre)".format(name)))
 if input("¿Tienes cansancio? ").lower() == 's':
-    kb.tell(expr("Sintoma({}, Cansancio)".format(name)))"""
+    kb.tell(expr("Sintoma({}, Cansancio)".format(name)))
 
-# Preguntas de niveles de laboratorio
-
-
-def interpretar_glucosa(nivel):
-    if nivel > 125:
-        return "GlucosaAlta"
-    elif nivel < 70:
-        return "GlucosaBaja"
-    else:
-        return "GlucosaNormal"
-
-
-def interpretar_hemoglobina(nivel):
-    if nivel < 12:
-        return "HemoglobinaBaja"
-    elif nivel > 18:
-        return "HemoglobinaAlta"
-    else:
-        return "HemoglobinaNormal"
-
-
-def interpretar_colesterol(nivel):
-    if nivel > 200:
-        return "ColesterolAlto"
-    else:
-        return "ColesterolNormal"
-
-
-def interpretar_trigliceridos(nivel):
-    if nivel > 150:
-        return "TrigliceridosAltos"
-    else:
-        return "TrigliceridosNormales"
-
-
-def interpretar_creatinina(nivel):
-    if nivel > 1.3:
-        return "CreatininaAlta"
-    else:
-        return "CreatininaNormal"
-
+# ========================================
+# Preguntas de Makenly
+# ========================================
+if input("¿Tienes dolor de cabeza? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, DolorCabeza)".format(name)))
+if input("¿Tienes dolor de garganta? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, DolorGarganta)".format(name)))
+if input("¿Tienes náuseas? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, Nauseas)".format(name)))
+if input("¿Tienes dolor de estómago? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, DolorEstomago)".format(name)))
+if input("¿Tienes dolor de espalda? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, DolorEspalda)".format(name)))
+if input("¿Tienes cansancio? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, Cansancio)".format(name)))
+if input("¿Tienes tos? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, Tos)".format(name)))
+if input("¿Tienes dificultad para respirar? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, DificultadRespirar)".format(name)))
+if input("¿Tienes dolor articular? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, DolorArticular)".format(name)))
+if input("¿Tienes inflamación? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, Inflamacion)".format(name)))
+if input("¿Has perdido el olfato? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, PerdidaOlfato)".format(name)))
+if input("¿Tienes fiebre? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, Fiebre)".format(name)))
+if input("¿Tienes dolor de oídos? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, DolorOidos)".format(name)))
+if input("¿Tienes visión borrosa? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, VisionBorroza)".format(name)))
+if input("¿Tienes vómito? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, Vomito)".format(name)))
+if input("¿Tienes dolor abdominal? (s/n) ").lower() == 's':
+    kb.tell(expr("Sintoma({}, DolorAbdomen)".format(name)))
 
 nivel_glucosa = float(input("¿Cuál es tu nivel de glucosa (mg/dL)? "))
 resultado_glucosa = interpretar_glucosa(nivel_glucosa)
 if resultado_glucosa != "GlucosaNormal":
     kb.tell(expr("Sintoma({}, {})".format(name, resultado_glucosa)))
-
 nivel_hemoglobina = float(input("¿Cuál es tu nivel de hemoglobina (g/dL)? "))
 resultado_hemoglobina = interpretar_hemoglobina(nivel_hemoglobina)
 if resultado_hemoglobina != "HemoglobinaNormal":
     kb.tell(expr("Sintoma({}, {})".format(name, resultado_hemoglobina)))
-
 nivel_colesterol = float(
     input("¿Cuál es tu nivel de colesterol total (mg/dL)? "))
 resultado_colesterol = interpretar_colesterol(nivel_colesterol)
 if resultado_colesterol != "ColesterolNormal":
     kb.tell(expr("Sintoma({}, {})".format(name, resultado_colesterol)))
-
 nivel_trigliceridos = float(
     input("¿Cuál es tu nivel de triglicéridos (mg/dL)? "))
 resultado_trigliceridos = interpretar_trigliceridos(nivel_trigliceridos)
 if resultado_trigliceridos != "TrigliceridosNormales":
     kb.tell(expr("Sintoma({}, {})".format(name, resultado_trigliceridos)))
-
 nivel_creatinina = float(input("¿Cuál es tu nivel de creatinina (mg/dL)? "))
 resultado_creatinina = interpretar_creatinina(nivel_creatinina)
 if resultado_creatinina != "CreatininaNormal":
